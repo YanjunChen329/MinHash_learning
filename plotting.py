f854c044d272fab9ed3c0114a2c0cd86707294bd
YanjunChen329 <chenyanjun0329@outlook.com>
2019-10-17 10:57:39 -0500

    change plotting

--- plotting.py
+++ plotting.py
@@ -18,19 +18,29 @@ def plot_experiment(K_arr, L, metric="acc", baseline=True):
     if baseline:
         baseline_acc = np.loadtxt(join(record_dir, "{}_L{}.txt".format(metric, L)))
         baseline_val = np.loadtxt(join(record_dir, "val_{}_L{}.txt".format(metric, L)))
-        plt.plot(np.arange(1, baseline_acc.shape[0] + 1), baseline_acc, label="Train(baseline)", color="r")
-        plt.plot(np.arange(1, baseline_val.shape[0] + 1), baseline_val, label="Test(baseline)", color="g")
+
+        step = int(baseline_acc.shape[0] / baseline_val.shape[0])
+        train_axis = np.arange(1, baseline_acc.shape[0] + 1)
+        val_axis = np.arange(1, baseline_acc.shape[0] + 1, step)
+
+        plt.plot(train_axis, baseline_acc, label="Train(baseline)", color="r", zorder=1)
+        plt.plot(val_axis, baseline_val, label="Test(baseline)", color="g", zorder=2)
         fig_name = join(record_dir, "pic_{}_L{}.png".format(metric, L))
 
     for k in K_arr:
         acc = np.loadtxt(join(record_dir, "{}_K{}_L{}.txt".format(metric, k, L)))
         val_acc = np.loadtxt(join(record_dir, "val_{}_K{}_L{}.txt".format(metric, k, L)))
-        plt.plot(np.arange(1, acc.shape[0] + 1), acc, label="Train(K={})".format(k))
-        plt.plot(np.arange(1, val_acc.shape[0] + 1), val_acc, label="Test(K={})".format(k))
+
+        step = int(acc.shape[0] / val_acc.shape[0])
+        train_axis = np.arange(1, baseline_acc.shape[0] + 1)
+        val_axis = np.arange(1, baseline_acc.shape[0] + 1, step)
+
+        plt.plot(train_axis, acc, label="Train(K={})".format(k), zorder=1)
+        plt.plot(val_axis, val_acc, label="Test(K={})".format(k), zorder=2)
         fig_name = join(record_dir, "pic_{}_K{}_L{}.txt".format(metric, k, L))
 
     plt.title(title)
-    plt.xlabel("Epoch")
+    plt.xlabel("Iteration")
     plt.legend()
     plt.savefig(fig_name, bbox_inches='tight')
     plt.close()
